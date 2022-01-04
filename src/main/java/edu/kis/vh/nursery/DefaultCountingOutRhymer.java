305ca6fa80f2c3a740ac327e3d80255ce0c5122f
Kruz00 <hubertkrauza@gmail.com>
2022-01-04 22:15:08 +0100

    1.4.4 Literały na stałe - Hubert

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int TAB_SIZE = 12;
+    public static final int ERR_CODE = -1;
+    public static final int INDEX_EMPTY = -1;
+    private int[] numbers = new int[TAB_SIZE];
 
-    public int total = -1;
+    public int total = INDEX_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INDEX_EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == TAB_SIZE-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERR_CODE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERR_CODE;
         return numbers[total--];
     }
 
