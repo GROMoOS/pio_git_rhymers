3aff59f436c8a3b1ed1011a3aec073dce74b0b1e
Oskar <80629512+GROMoOS@users.noreply.github.com>
2022-01-10 11:40:04 +0100

    2.1 move defaultRyhmer to IntArrayStack and change defaultRhymer to use IntLinkedList instead

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,72 +1,32 @@
 package edu.kis.vh.nursery;
 
-/**
- * Specifies default counting out rhymer.
- */
+
+import edu.kis.vh.nursery.list.IntLinkedList;
+
 public class DefaultCountingOutRhymer {
+    private IntLinkedList intLinkedList;
 
-    public static final int MAX_NUMBERS_SIZE = 12;
-    public static final int MAX_NUMBERS_INDEX = MAX_NUMBERS_SIZE-1;
-    public static final int ERR_CODE = -1;
-    public static final int INDEX_EMPTY = -1;
-
-    private final int[] numbers = new int[MAX_NUMBERS_SIZE];
-
-    private int total = INDEX_EMPTY;
-
-    /**
-     * Get value of total.
-     * @return total.
-     */
-    public int getTotal() {
-        return total;
+    public DefaultCountingOutRhymer(){
+        this.intLinkedList = new IntLinkedList();
     }
 
-    /**
-     * If number is not full add value to numbers.
-     * @param in value added into numbers array.
-     */
     public void countIn(int in) {
-        if (!isFull())
-            numbers[++total] = in;
+        intLinkedList.push(in);
     }
 
-    /**
-     * Checks if numbers is empty.
-     * @return true when numbers is empty.
-     */
     public boolean callCheck() {
-        return total == INDEX_EMPTY;
+        return intLinkedList.isEmpty();
     }
 
-    /**
-     * Checks if numbers is full.
-     * @return true when numbers is full.
-     */
     public boolean isFull() {
-        return total == MAX_NUMBERS_INDEX;
+        return intLinkedList.isFull();
     }
 
-    /**
-     * If numbers is not empty return top number.
-     * @return if numbers is empty ERR_CODE,
-     *         else top number from numbers.
-     */
-    protected int peekaboo() {
-        if (callCheck())
-            return ERR_CODE;
-        return numbers[total];
+    public int peekaboo() {
+        return intLinkedList.top();
     }
 
-    /**
-     * If numbers is not empty return top number and decrease total by 1.
-     * @return if numbers is empty ERR_CODE,
-     *         else top number from numbers.
-     */
     public int countOut() {
-        if (callCheck())
-            return ERR_CODE;
-        return numbers[total--];
+        return intLinkedList.pop();
     }
-
 }
---
+++ src/main/java/edu/kis/vh/nursery/IntArrayStack.java
@@ -0,0 +1,43 @@
+package edu.kis.vh.nursery;
+
+public class IntArrayStack {
+
+    public static final int MAX_NUMBERS_SIZE = 12;
+    public static final int MAX_NUMBERS_INDEX = MAX_NUMBERS_SIZE-1;
+    public static final int ERR_CODE = -1;
+    public static final int INDEX_EMPTY = -1;
+
+    private final int[] numbers = new int[MAX_NUMBERS_SIZE];
+
+    private int total = INDEX_EMPTY;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void countIn(int in) {
+        if (!isFull())
+            numbers[++total] = in;
+    }
+
+    public boolean callCheck() {
+        return total == INDEX_EMPTY;
+    }
+
+    public boolean isFull() {
+        return total == MAX_NUMBERS_INDEX;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return ERR_CODE;
+        return numbers[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return ERR_CODE;
+        return numbers[total--];
+    }
+
+}
